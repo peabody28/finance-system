1a09a7b40f6b06c8eb70f16de656d85a07f62dec
Maksim Gorbachewski <gorbachewski.m@gmail.com>
2023-03-29 11:55:23 +0300

    wallet integration tests begin

---
+++ wallet.tests/Integration/GetWalletTest.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using NUnit.Framework;
+using System.Net;
+
+namespace wallet.tests.Integration
+{
+    public class GetWalletTest
+    {
+        private WebApplicationFactory<Program> factory;
+
+        private HttpClient client;
+
+        [SetUp]
+        public void Setup()
+        {
+            factory = new WebApplicationFactory<Program>();
+            client = factory.CreateClient();
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            client?.Dispose();
+            factory?.Dispose();
+        }
+
+        [Test]
+        public async Task TestUnauthorizedGetWallets()
+        {
+            // Act
+            var result = await client.GetAsync("/wallet/");
+
+            // Assert
+            Assert.That(result.StatusCode, Is.EqualTo(HttpStatusCode.Unauthorized));
+        }
+
+        [Test]
+        public async Task TestUnauthorizedGetWalletByNumber()
+        {
+            // Act 
+            var result = await client.GetAsync("/wallet/<some_number>");
+
+            // Assert
+            Assert.That(result.StatusCode, Is.EqualTo(HttpStatusCode.Unauthorized));
+        }
+
+        // auth 
+        // https://mazeez.dev/posts/auth-in-integration-tests
+    }
+}
