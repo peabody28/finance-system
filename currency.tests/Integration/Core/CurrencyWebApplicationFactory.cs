d76a1b95b2f6f50cdbbe77fdae65177f01a25fa9
Maksim Harbacheuski <gorbachewski.m@gmail.com>
2023-03-30 20:03:23 +0300

    use sqlite helper class for integration tests

--- currency.tests/Integration/Core/CurrencyWebApplicationFactory.cs
+++ currency.tests/Integration/Core/CurrencyWebApplicationFactory.cs
@@ -1,40 +1,17 @@
-﻿using Microsoft.Data.Sqlite;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
-using System.Data.Common;
-using Testing.Integration.Core;
+﻿using Microsoft.EntityFrameworkCore;
 using currency.tests.Integration.Core.Constants;
+using Testing.Integration.Helper.Sqlite;
 
 namespace currency.tests.Integration.Core
 {
-    public class CurrencyWebApplicationFactory<TProgram, TContext> : AbstractWebApplicationFactory<TProgram, TContext>
+    public class CurrencyWebApplicationFactory<TProgram, TContext> : SqliteContextWebApplicationFactory<TProgram, TContext>
         where TProgram : class
         where TContext : DbContext
     {
-        public CurrencyWebApplicationFactory() 
+        public CurrencyWebApplicationFactory()
         {
             UserName = TestUserConstants.UserName;
             UserRole = TestUserConstants.UserRole;
-        }
-
-        protected override void AddDbConnectionService(IServiceCollection services)
-        {
-            services.AddSingleton<DbConnection>(container =>
-            {
-                var connection = new SqliteConnection("DataSource=:memory:");
-                connection.Open();
-
-                return connection;
-            });
-        }
-
-        protected override void AddDbContextService(IServiceCollection services)
-        {
-            services.AddDbContext<TContext>((container, options) =>
-            {
-                var connection = container.GetRequiredService<DbConnection>();
-                options.UseSqlite(connection);
-            });
         }
     }
 }
--- wallet.tests/Integration/Core/WalletWebApplicationFactory.cs
+++ wallet.tests/Integration/Core/WalletWebApplicationFactory.cs
@@ -1,40 +1,17 @@
-﻿using Microsoft.Data.Sqlite;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
-using System.Data.Common;
-using Testing.Integration.Core;
+﻿using Microsoft.EntityFrameworkCore;
+using Testing.Integration.Helper.Sqlite;
 using wallet.tests.Integration.Core.Constants;
 
 namespace wallet.tests.Integration.Core
 {
-    public class WalletWebApplicationFactory<TProgram, TContext> : AbstractWebApplicationFactory<TProgram, TContext>
+    public class WalletWebApplicationFactory<TProgram, TContext> : SqliteContextWebApplicationFactory<TProgram, TContext>
         where TProgram : class
         where TContext : DbContext
     {
-        public WalletWebApplicationFactory() 
+        public WalletWebApplicationFactory()
         {
             UserName = TestUserConstants.UserName;
             UserRole = TestUserConstants.UserRole;
-        }
-
-        protected override void AddDbConnectionService(IServiceCollection services)
-        {
-            services.AddSingleton<DbConnection>(container =>
-            {
-                var connection = new SqliteConnection("DataSource=:memory:");
-                connection.Open();
-
-                return connection;
-            });
-        }
-
-        protected override void AddDbContextService(IServiceCollection services)
-        {
-            services.AddDbContext<TContext>((container, options) =>
-            {
-                var connection = container.GetRequiredService<DbConnection>();
-                options.UseSqlite(connection);
-            });
         }
     }
 }
